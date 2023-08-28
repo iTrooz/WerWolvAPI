fa75d9e4edcacdebd805f90f4bc6f829652cfcaf
iTrooz <hey@itrooz.fr>
2023-08-29 00:46:59 +0200

    speedup plcli metadata extraction by using asyncio

--- api/impl/imhex/store.py
+++ api/impl/imhex/store.py
@@ -4,13 +4,15 @@ import subprocess
 import shutil
 import hashlib
 from pathlib import Path
+import asyncio
+import os
 
 import config
 
 
 store_folders = [ "patterns", "includes", "magic", "constants", "yara", "encodings", "nodes", "themes" ]
 
-def get_pattern_metadata(file_path: str, type_: str) -> str:
+async def get_pattern_metadata(file_path: str, type_: str) -> str:
     """
     Get the associated metadata value of a pattern file, using the `plcli` tool. Returns None if the tool is not found
 
@@ -20,10 +22,68 @@ def get_pattern_metadata(file_path: str, type_: str) -> str:
     """
 
     std_folder = Path(config.Common.CONTENT_FOLDER) / "imhex" / "includes"
-    try:
-        return subprocess.check_output(["plcli", "info", file_path, "-t", type_, "-I", std_folder]).decode()
-    except subprocess.CalledProcessError as e:
-        return ""
+    
+    # run plcli process
+    process = await asyncio.create_subprocess_exec("plcli", "info", file_path, "-t", type_, "-I", std_folder, stdout=asyncio.subprocess.PIPE)
+    await process.wait()
+
+    stdout, _ = await process.communicate()
+
+    if process.returncode != 0:
+        print(stdout.decode())
+        print(f"plcli command exited with return code {process.returncode}")
+        return None
+
+    return stdout.decode()
+
+async def semaphore_wrapper(task, semaphore):
+    """
+    Wrap a task inside a semaphore, to limit tasks concurrency
+    """
+    async with semaphore:
+        await task
+
+class PatternMetadata:
+    def __init__(self):
+        self.filepath = ""
+        self.description = ""
+        self.authors = []
+        self.mime = ""
+
+    async def set_description(self):
+        self.description = (await get_pattern_metadata(self.filepath, "description")).strip()
+
+    async def set_author(self):
+        self.authors = (await get_pattern_metadata(self.filepath, "authors")).strip().split("\n")
+
+    async def set_mime(self):
+        self.mime = (await get_pattern_metadata(self.filepath, "mime")).strip()
+
+    def __repr__(self):
+        return f"PatternMetadata(filepath={self.filepath},description={self.description},authors={self.authors})"
+    
+async def get_all_pattern_metadata(folder: str) -> Dict[str, PatternMetadata]:
+    """
+    Get all metadata (authors and description) for all patterns in a given folder
+    """
+
+    # Max number of tasks (commands) that will be run at the same time
+    sem = asyncio.Semaphore(50)
+
+    mds = {}
+    tasks = []
+    for file in os.listdir(folder):
+        md = PatternMetadata()
+        mds[file] = md
+        md.filepath = Path(folder) / file
+
+        tasks.append(semaphore_wrapper(md.set_author(), sem))
+        tasks.append(semaphore_wrapper(md.set_description(), sem))
+        tasks.append(semaphore_wrapper(md.set_mime(), sem))
+        
+    await asyncio.gather(*tasks)
+
+    return mds
 
 def is_plcli_found() -> bool:
     """
@@ -35,7 +95,12 @@ def gen_store(root_url: str) -> Dict[str, List[Dict]]:
     """
     Generate an object representing the ImHex store, that can be returned by /imhex/store
     """
-    plcli_found = is_plcli_found()
+
+    if is_plcli_found():
+        patterns_mds = asyncio.run(get_all_pattern_metadata(Path(".") / "content" / "imhex" / "patterns"))
+    else:
+        patterns_mds = None
+
     store = {}
     for folder in store_folders:
         store[folder] = []
@@ -53,10 +118,11 @@ def gen_store(root_url: str) -> Dict[str, List[Dict]]:
                         "desc": "",
                         "mime": "",
                         }
-                    if folder == "patterns" and plcli_found:
-                        data["authors"] = list(filter(None, get_pattern_metadata(file, "authors").split("\n")))
-                        data["desc"] = get_pattern_metadata(file, "description")
-                        data["mime"] = get_pattern_metadata(file, "mime")
+                    if folder == "patterns" and patterns_mds:
+                        md = patterns_mds[file.name]
+                        data["authors"] = md.authors
+                        data["desc"] = md.description
+                        data["mime"] = md.mime
                     store[folder].append(data)
 
     return store
